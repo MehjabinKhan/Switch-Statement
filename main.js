3a6d60e68ba8917a9c00e6241481da41d55da13d
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-31 23:43:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+// Switch Statement
+var weekEnd = "Saturday";
+switch (weekEnd) {
+    case "Sunday":
+        console.log("Spend time with family.");
+        break;
+    case "Saturday":
+        console.log("Explore the art of gardening.");
+        break;
+    default:
+        console.log("Find a new hobby.");
+}
+var finalGrade = "A+";
+switch (finalGrade) {
+    case "A":
+        console.log("Showed great performance");
+        break;
+    case "B":
+        console.log("Satisfactory performance");
+        break;
+    default:
+        console.log("Outstanding performance");
+}
+var gotAdmission = "Karachi University";
+switch (gotAdmission) {
+    case "Iqra University":
+        console.log("Congratulations!You got admission in Computer Science");
+        break;
+    case "Karachi University":
+        console.log("Congratulations!You got admission in Public Health");
+        break;
+    case "Jinnah University For Women":
+        console.log("Congratulations!You got admission in Microbiology");
+        break;
+    default:
+        console.log("Sorry! You are not eligible");
+}
